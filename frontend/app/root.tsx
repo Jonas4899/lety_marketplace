a1f4204840b67d7873d814e0306ab335172f59bf
Jonathan David Salcedo Diaz <jonatandavidsal@gmail.com>
2025-03-03 13:33:33 -0500

    feat: :zap: Add Material UI as a dependency

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -8,6 +8,10 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
