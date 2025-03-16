82cdddb5189af1a455bf922e316febc7a9656c05
JuanFelipeRomero <juanfeliperomerocastro09@gmail.com>
2025-03-16 14:42:14 -0500

    feat: add register users endpoint in the backend

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -9,5 +9,6 @@ export default [
   index("routes/home.tsx"),
   layout("layouts/AuthLayout/index.tsx", [
     route("registrar-clinica", "routes/RegisterVeterinary/BasicVetInfo.tsx"),
+    route("registrar-dueño-mascota", "routes/RegisterOwner/BasicOwnerInfo.tsx"),
   ]),
 ] satisfies RouteConfig;
