a6da9f57ab1769f1f607c2970c0fe3a77454d1cb
JuanFelipeRomero <juanfeliperomerocastro09@gmail.com>
2025-05-14 10:04:30 -0500

    add frontend routes to solve CORS error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,28 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import fs from "fs";
-import cookieParser from "cookie-parser";
-import routes from "./routes/index.js";
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+import cookieParser from 'cookie-parser';
+import routes from './routes/index.js';
 
 // Configurar variables de entorno
 dotenv.config();
 
-if (!fs.existsSync("uploads/")) {
-  fs.mkdirSync("uploads/");
+if (!fs.existsSync('uploads/')) {
+  fs.mkdirSync('uploads/');
 }
 
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // URL de tu frontend - both localhost and IP
+    origin: [
+      'https://lety-marketplace.vercel.app/',
+      'https://lety-marketplace-juan-romeros-projects-f4ebe484.vercel.app/',
+      'https://lety-marketplace-git-main-juan-romeros-projects-f4ebe484.vercel.app/',
+      'https://lety-marketplace-p5hthhw13-juan-romeros-projects-f4ebe484.vercel.app/',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+    ], // URL de tu frontend - both localhost and IP
     credentials: true, // Permitir cookies
   })
 );
@@ -25,10 +32,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 // Usar las rutas definidas en el índice
-app.use("/", routes);
+app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
-if (process.env.NODE_ENV !== "test") {
+if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
   });
